2e7e9eb2e26f66e2373f6075b8840ca33b414ccc
Umut Karcı <cediddi@gmail.com>
2016-12-19 01:52:41 +0300

    wrong operator.

--- mplibs/wemos_shields/oled.py
+++ mplibs/wemos_shields/oled.py
@@ -10,7 +10,7 @@ oled_conn = SSD1306_I2C(64, 48, i2c)
 
 def ez_text(fb):
     oled_conn.fill(0)
-    if type(fb) != str:
+    if type(fb) == str:
         fb = chunk(fb[:48], 8)
     for text, row in zip(fb, range(0, 48, 8)):
         oled_conn.text("{:8.8}".format(str(text)), 0, row)
